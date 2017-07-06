611a96cfd3ffef5aecd35347a27dabe888eddf68
Melissa Martinez <melmar12@live.com>
2017-07-05 20:45:41 -0500

    configured twitter api

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-  =
+var Twitter = require('twit');
+
+var config = require('./config');
+var twitter = new Twitter(config);
+
+
+twitter.get('search/tweets', { q: 'banana since:2011-07-11', count: 1 }, function(err, data, response) {
+  console.log(data)
+});
+
+twitter.post('statuses/update', { status: '🤖' }, function(err, data, response) {
+  console.log(data)
+});
